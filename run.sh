cb31af64cdfb582993138953aea56500f6c8d6ca
KNOTSREPUS <knotsrepus+github@protonmail.com>
2021-07-17 15:29:08 +0100

    Update cluster mode configuration

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ then
   echo "No arguments provided."
 elif [ "$1" = "--cluster" ]
 then
+  export AWS_ACCESS_KEY_ID="$4"
+  export AWS_SECRET_ACCESS_KEY="$5"
   echo "Using AWS cluster..."
   ray up --no-config-cache -y config.yaml
   echo "Ray cluster started."
