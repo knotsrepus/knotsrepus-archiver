b2c7f583607aa3fc29f5ee011b136f77ea118298
KNOTSREPUS <knotsrepus+github@protonmail.com>
2021-10-09 23:30:11 +0100

    Implement type coercion for query/path params

--- src/knotsrepus-api-lambda/rest.py
+++ src/knotsrepus-api-lambda/rest.py
@@ -18,7 +18,37 @@ def route_is_defined(path):
 def dispatch(path, *args, **kwargs):
     fn, path_params = match_route(path)
     kwargs.update(path_params)
+    kwargs = coerce_param_types(kwargs)
     return fn(*args, **kwargs)
+
+
+def coerce_param_types(params: dict):
+    for key, value in params.items():
+        try:
+            value = int(value)
+            params[key] = value
+            continue
+        except ValueError:
+            # Okay as an attempt will be made to parse as a float or bool before continuing as a string.
+            pass
+
+        try:
+            value = float(value)
+            params[key] = value
+            continue
+        except ValueError:
+            # Okay as an attempt will be made to parse as a bool before continuing as a string.
+            pass
+
+        if value.lower() == "true":
+            params[key] = True
+        elif value.lower() == "false":
+            params[key] = False
+        else:
+            # Keep value as a string.
+            pass
+
+    return params
 
 
 def match_route(path):
