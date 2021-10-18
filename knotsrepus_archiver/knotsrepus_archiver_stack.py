939b883608847ea0b4d4056a4c120cfbc059838d
KNOTSREPUS <knotsrepus+github@protonmail.com>
2021-10-18 16:28:11 +0100

    Add ArchiveMetadataByDummyChronological GSI

--- knotsrepus_archiver/knotsrepus_archiver_stack.py
+++ knotsrepus_archiver/knotsrepus_archiver_stack.py
@@ -139,12 +139,12 @@ class KnotsrepusArchiverStack(core.Stack):
             max_capacity=1000
         ).scale_on_utilization(target_utilization_percent=70)
 
-        # metadata_table.add_global_secondary_index(
-        #     index_name="ArchiveMetadataByDummyChronological",
-        #     partition_key=dynamodb.Attribute(name="dummy", type=dynamodb.AttributeType.STRING),
-        #     sort_key=dynamodb.Attribute(name="created_utc", type=dynamodb.AttributeType.NUMBER),
-        #     projection_type=dynamodb.ProjectionType.ALL
-        # )
+        metadata_table.add_global_secondary_index(
+            index_name="ArchiveMetadataByDummyChronological",
+            partition_key=dynamodb.Attribute(name="dummy", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="created_utc", type=dynamodb.AttributeType.NUMBER),
+            projection_type=dynamodb.ProjectionType.ALL
+        )
         # metadata_table.add_global_secondary_index(
         #     index_name="ArchiveMetadataByDummyScore",
         #     partition_key=dynamodb.Attribute(name="dummy", type=dynamodb.AttributeType.STRING),
@@ -176,16 +176,16 @@ class KnotsrepusArchiverStack(core.Stack):
             projection_type=dynamodb.ProjectionType.ALL
         )
 
-        # metadata_table.auto_scale_global_secondary_index_read_capacity(
-        #     index_name="ArchiveMetadataByDummyChronological",
-        #     min_capacity=5,
-        #     max_capacity=1000,
-        # ).scale_on_utilization(target_utilization_percent=70)
-        # metadata_table.auto_scale_global_secondary_index_write_capacity(
-        #     index_name="ArchiveMetadataByDummyChronological",
-        #     min_capacity=5,
-        #     max_capacity=1000,
-        # ).scale_on_utilization(target_utilization_percent=70)
+        metadata_table.auto_scale_global_secondary_index_read_capacity(
+            index_name="ArchiveMetadataByDummyChronological",
+            min_capacity=5,
+            max_capacity=1000,
+        ).scale_on_utilization(target_utilization_percent=70)
+        metadata_table.auto_scale_global_secondary_index_write_capacity(
+            index_name="ArchiveMetadataByDummyChronological",
+            min_capacity=5,
+            max_capacity=1000,
+        ).scale_on_utilization(target_utilization_percent=70)
         # metadata_table.auto_scale_global_secondary_index_read_capacity(
         #     index_name="ArchiveMetadataByDummyScore",
         #     min_capacity=5,
